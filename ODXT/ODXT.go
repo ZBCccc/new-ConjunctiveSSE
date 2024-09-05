24165455c886ea2a6d4ede64d383ea8940ef5b9d
Zbcccc <1641671577@qq.com>
2024-09-05 16:18:42 +0800

    优化加密时间

--- ODXT/ODXT.go
+++ ODXT/ODXT.go
@@ -4,14 +4,20 @@ import (
 	"ConjunctiveSSE/util"
 	"context"
 	"crypto/rand"
-	"github.com/bits-and-blooms/bloom/v3"
-	"go.mongodb.org/mongo-driver/bson"
+	"encoding/gob"
+	"fmt"
 	"log"
 	"math/big"
+	"os"
 	"time"
 
-	"go.mongodb.org/mongo-driver/mongo"
+	"github.com/bits-and-blooms/bloom/v3"
+	mapset "github.com/deckarep/golang-set/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 //var PlaintextDB *mongo.Collection
@@ -36,7 +42,7 @@ type UpdatePayload struct {
 	Alpha   *big.Int
 }
 
-func (odxt *ODXT) DBSetup(db_name string) error {
+func (odxt *ODXT) DBSetup(dbName string) error {
 	// 生成4个32字节长度的随机私钥
 	keyLen := 32
 
@@ -78,7 +84,7 @@ func (odxt *ODXT) DBSetup(db_name string) error {
 	}
 
 	// 获取数据库和集合的句柄
-	odxt.PlaintextDB = client.Database(db_name)
+	odxt.PlaintextDB = client.Database(dbName)
 
 	return nil
 }
@@ -89,6 +95,8 @@ func (odxt *ODXT) CiphertextGenPhase() {
 
 	uploadList := make([]UpdatePayload, UploadListMaxLength)
 	encryptTimeList := make([]time.Duration, 0)
+	cipherNum := 0
+	var encryptTimeTotal time.Duration
 
 	// 从MongoDB数据库中获取名为"id_keywords"的集合
 	collection := plaintextDB.Collection("id_keywords")
@@ -112,15 +120,107 @@ func (odxt *ODXT) CiphertextGenPhase() {
 
 	// 读取所有记录
 	for _, keywordId := range keywordIds {
-		ids := keywordId["id"].([]string)
-		keyword := keywordId["keyword"].(string)
+		// ids := keywordId["val_set"].([]string)
+		valSet, ok := keywordId["val_set"].(primitive.A)
+		if !ok {
+			log.Fatal("val_set is not of type primitive.A")
+		}
+		var ids []string
+		for _, v := range valSet {
+			if str, ok := v.(string); ok {
+				ids = append(ids, str)
+			} else {
+				log.Fatal("val_set contains non-string value")
+			}
+		}
+		ids = removeDuplicates(ids)
+		keyword := keywordId["k"].(string)
 		encryptTime, keywordCipher, err := odxt.Encrypt(keyword, ids, 1)
 		if err != nil {
 			log.Fatal(err)
 		}
 		uploadList = append(uploadList, keywordCipher...)
 		encryptTimeList = append(encryptTimeList, encryptTime)
+
+		// 如果上传列表的长度达到最大限制， 则将其写入数据库
+		if len(uploadList) >= UploadListMaxLength {
+			// 写入文件
+			err = WriteUploadList(uploadList, "uploadList.txt")
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// 清空上传列表
+			uploadList = make([]UpdatePayload, UploadListMaxLength)
+		}
+		cipherNum += len(keywordCipher)
+		encryptTimeTotal += encryptTime
+		fmt.Println("Encrypting", keyword, "with", len(ids), "ids")
+		fmt.Println("Total number of ciphertexts:", len(keywordCipher))
+		fmt.Println("Total time for encrypting", encryptTime)
 	}
+
+	// 如果上传列表不为空， 则将其写入数据库
+	if len(uploadList) > 0 {
+		// 写入文件
+		err = WriteUploadList(uploadList, "uploadList.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// 写入加密时间
+	err = WriteEncryptTime(encryptTimeList, "encryptTime.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Total number of ciphertexts:", cipherNum)
+	fmt.Println("Total time for encrypting", encryptTimeTotal)
+}
+
+func WriteEncryptTime(encryptTimeList []time.Duration, fileName string) error {
+	// 创建或打开文件
+	file, err := os.Create(fileName)
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return err
+	}
+	defer file.Close()
+
+	// 将时间逐个写入文件
+	for _, encryptTime := range encryptTimeList {
+		_, err := file.WriteString(encryptTime.String() + "\n")
+		if err != nil {
+			fmt.Println("Error writing time:", err)
+			return err
+		}
+	}
+	return nil
+}
+
+// WriteUploadList 写入文件，将[]UpdatePayload写入文件
+func WriteUploadList(uploadList []UpdatePayload, fileName string) error {
+	// 创建或打开文件
+	file, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return err
+	}
+	defer file.Close()
+
+	// 创建编码器
+	encoder := gob.NewEncoder(file)
+
+	// 将结构体切片逐个写入文件
+	for _, payload := range uploadList {
+		err := encoder.Encode(payload)
+		if err != nil {
+			fmt.Println("Error encoding payload:", err)
+			return err
+		}
+	}
+	return nil
 }
 
 func (odxt *ODXT) Encrypt(keyword string, ids []string, operation int) (time.Duration, []UpdatePayload, error) {
@@ -128,13 +228,16 @@ func (odxt *ODXT) Encrypt(keyword string, ids []string, operation int) (time.Dur
 	p, g := odxt.p, odxt.g
 
 	var encryptedTime time.Duration
-	keywordsCipher := make([]UpdatePayload, len(ids))
-	start := time.Now()
+	keywordsCipher := make([]UpdatePayload, 0)
+
+	_, ok := odxt.UpdateCnt[keyword]
+	if !ok {
+		odxt.UpdateCnt[keyword] = 0
+	}
+
 	for _, id := range ids {
-		_, ok := odxt.UpdateCnt[keyword]
-		if !ok {
-			odxt.UpdateCnt[keyword] = 0
-		}
+		start := time.Now()
+
 		odxt.UpdateCnt[keyword]++
 
 		// address = PRF(kt, w||wc||0)
@@ -146,30 +249,30 @@ func (odxt *ODXT) Encrypt(keyword string, ids []string, operation int) (time.Dur
 		}
 
 		// val = PRF(kt, w||wc||1) xor (id||op)
-		val, err := util.PrfF(kt, append(wWc, big.NewInt(int64(1)).Bytes()...))
-		if err != nil {
-			log.Println(err)
-			return time.Second, nil, err
-		}
-		val, err = util.BytesXORWithOp(val, []byte(id), operation)
-		if err != nil {
-			log.Println(err)
-			return time.Second, nil, err
-		}
+		val, _ := util.PrfF(kt, append(wWc, big.NewInt(int64(1)).Bytes()...))
+		// if err != nil {
+		// 	log.Println(err)
+		// 	return time.Second, nil, err
+		// }
+		val, _ = util.BytesXORWithOp(val, []byte(id), operation)
+		// if err != nil {
+		// 	log.Println(err)
+		// 	return time.Second, nil, err
+		// }
 
 		// alpha = Fp(ky, id||op) * Fp(kz, w||wc)^-1
-		alpha, alpha1, err := util.ComputeAlpha(ky, kz, []byte(id), operation, wWc, p, g)
-		if err != nil {
-			log.Println(err)
-			return time.Second, nil, err
-		}
+		alpha, alpha1, _ := util.ComputeAlpha(ky, kz, []byte(id), operation, wWc, p, g)
+		// if err != nil {
+		// 	log.Println(err)
+		// 	return time.Second, nil, err
+		// }
 
 		// xtag = g^{Fp(Kx, w)*Fp(Ky, id||op)} mod p
-		C, err := util.PrfFp(kx, []byte(keyword), p, g)
-		if err != nil {
-			log.Println(err)
-			return time.Second, nil, err
-		}
+		C, _ := util.PrfFp(kx, []byte(keyword), p, g)
+		// if err != nil {
+		// 	log.Println(err)
+		// 	return time.Second, nil, err
+		// }
 		A := new(big.Int).Mul(C, alpha1)
 		xtag := new(big.Int).Exp(g, A, p)
 
@@ -182,4 +285,17 @@ func (odxt *ODXT) Encrypt(keyword string, ids []string, operation int) (time.Dur
 }
 
 func DeletionPhaseWithSearch(del_rate int) {
+}
+
+func removeDuplicates(intSlice []string) []string {
+	// 创建一个新的string集合
+	stringSet := mapset.NewSet[string]()
+
+	// 将切片中的元素添加到集合中
+	for _, v := range intSlice {
+		stringSet.Add(v)
+	}
+
+	// 转换为切片
+	return stringSet.ToSlice()
 }
--- benchmark/ODXT/main.go
+++ benchmark/ODXT/main.go
@@ -7,15 +7,15 @@ import (
 	"strings"
 )
 
-var test_db_name, test_phase, test_group string
-var del_rate int
+var testDbName, testPhase, testGroup string
+var delRate int
 
 func main() {
 	// Parse command line arguments
-	flag.StringVar(&test_db_name, "db", "ODXT", "database name")
-	flag.StringVar(&test_phase, "phase", "setup", "test phase")
-	flag.StringVar(&test_group, "group", "server", "test group")
-	flag.IntVar(&del_rate, "del_rate", 0, "delete rate")
+	flag.StringVar(&testDbName, "db", "Crime_USENIX_REV", "database name")
+	flag.StringVar(&testPhase, "phase", "c", "test phase")
+	flag.StringVar(&testGroup, "group", "server", "test group")
+	flag.IntVar(&delRate, "del_rate", 0, "delete rate")
 	flag.Parse()
 
 	// Run tests
@@ -27,18 +27,19 @@ func main() {
 
 func TestODXT() error {
 	fmt.Println("*********************************************")
-	fmt.Println("Test_on ", test_db_name, "del_rate", del_rate)
-	fmt.Println("Start test_group", test_group, "phase", test_phase)
+	fmt.Println("Test_on ", testDbName, "del_rate", delRate)
+	fmt.Println("Start test_group", testGroup, "phase", testPhase)
 	fmt.Println("Start initial db")
-	err := ODXT.DBSetup(test_db_name)
+	var odxt ODXT.ODXT
+	err := odxt.DBSetup(testDbName)
 	if err != nil {
 		fmt.Println("DBSetup error", err)
 		return err
 	}
-	if strings.Contains(test_phase, "c") {
-		ODXT.CiphertextGenPhase()
-	} else if strings.Contains(test_phase, "s") {
-		ODXT.DeletionPhaseWithSearch(del_rate)
+	if strings.Contains(testPhase, "c") {
+		odxt.CiphertextGenPhase()
+	} else if strings.Contains(testPhase, "s") {
+		ODXT.DeletionPhaseWithSearch(delRate)
 	}
 
 	return nil
