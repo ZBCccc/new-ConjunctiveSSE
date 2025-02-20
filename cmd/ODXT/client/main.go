4c12730962a59dbf89ab814db0ff0d171c0725b0
ZBCccc <1641671577@qq.com>
2025-02-20 12:41:15 +0800

    修改客户端ip配置

--- cmd/ODXT/client/main.go
+++ cmd/ODXT/client/main.go
@@ -18,7 +18,7 @@ import (
 )
 
 func main() {
-	c, err := client.NewODXTClient("localhost:50052")
+	c, err := client.NewODXTClient("10.12.188.9:50051")
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
