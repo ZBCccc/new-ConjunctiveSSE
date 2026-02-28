cd6dc57249ca328e003aa0067520bfcc7a5ef1b7
zhangbaichuan.02__dcar <zhangbaichuan.02@dcarlife.com>
2026-02-28 19:14:47 +0800

    chore: translate Chinese comments to English in scripts

--- scripts/gen-proto.sh
+++ scripts/gen-proto.sh
@@ -1,4 +1,4 @@
-# 生成proto文件
+# Generate proto files
 protoc --go_out=. --go-grpc_out=. pkg/ODXT/proto/odxt.proto
 protoc --go_out=. --go-grpc_out=. pkg/HDXT/proto/hdxt.proto
 protoc --go_out=. --go-grpc_out=. pkg/FDXT/proto/fdxt.proto
--- scripts/sql/v1.sql
+++ scripts/sql/v1.sql
@@ -2,20 +2,20 @@ create table if not exists xtag
 (
     id          bigint auto_increment comment 'id'
         primary key,
-    label varchar(255) not null comment '标签',
-    cipher_text varchar(255) not null comment '密文',
-    created_at  timestamp   default CURRENT_TIMESTAMP not null comment '创建时间',
-    updated_at  timestamp   default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    label varchar(255) not null comment 'label',
+    cipher_text varchar(255) not null comment 'ciphertext',
+    created_at  timestamp   default CURRENT_TIMESTAMP not null comment 'creation time',
+    updated_at  timestamp   default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment 'update time',
 )   comment 'xtag' collate = utf8mb4_bin;
 
 create table if not exists tset
 (
     id          bigint auto_increment comment 'id'
         primary key,
-    address varchar(255) not null comment '地址',
-    value varchar(255) not null comment '值',
+    address varchar(255) not null comment 'address',
+    value varchar(255) not null comment 'value',
     alpha varchar(255) not null comment 'alpha',
-    created_at  timestamp   default CURRENT_TIMESTAMP not null comment '创建时间',
-    updated_at  timestamp   default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    created_at  timestamp   default CURRENT_TIMESTAMP not null comment 'creation time',
+    updated_at  timestamp   default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment 'update time',
     
 )   comment 'tset' collate = utf8mb4_bin;
