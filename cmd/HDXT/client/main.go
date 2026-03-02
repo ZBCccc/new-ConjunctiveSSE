14a07d091ddacaef2c84383f091ad8bcad964a25
zhangbaichuan.02__dcar <zhangbaichuan.02@dcarlife.com>
2026-03-02 21:04:09 +0800

    refactor(hdxt): extract SetupPhase and UpdatePhase functions
    - Simplify main function to ~45 lines with clear workflow
    - Extract SetupPhase function for setup logic
    - Extract UpdatePhase function for update logic
    - Extract extractKeywords helper for keyword extraction
    - Extract saveResults helper for CSV output
    - Simplify SearchPhase parameters

--- cmd/HDXT/client/main.go
+++ cmd/HDXT/client/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"ConjunctiveSSE/pkg/Database"
 	"ConjunctiveSSE/pkg/HDXT"
 	"ConjunctiveSSE/pkg/HDXT/client"
 	"ConjunctiveSSE/pkg/utils"
@@ -15,6 +16,7 @@ import (
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
@@ -30,55 +32,56 @@ func main() {
 	}
 	defer c.Close()
 
-	// Execute experiment
-	// Get MongoDB database
 	hdxt := c.GetHDXT()
 	plaintextDB := hdxt.PlaintextDB
 	defer plaintextDB.Client().Disconnect(context.Background())
 
-	// Initialize
-	setupTimeList := make([]time.Duration, 0, 1000000)
+	// Setup Phase
+	log.Println("Setup Phase Start")
+	setupTime := time.Now()
+	SetupPhase(c, plaintextDB, *dbName)
+	log.Printf("Setup phase completed in %v\n", time.Since(setupTime))
 
-	// Get collection named 'id_keywords' from MongoDB database
+	// Update Phase
+	log.Println("Update Phase Start")
+	updateTime := time.Now()
+	UpdatePhase(c, plaintextDB, *dbName)
+	log.Printf("Update phase completed in %v\n", time.Since(updateTime))
+
+	// Search Phase
+	log.Println("Search Phase Start")
+	searchTime := time.Now()
+	SearchPhase(c, *dbName)
+	log.Printf("Search phase completed in %v\n", time.Since(searchTime))
+}
+
+func SetupPhase(c *client.HDXTClient, plaintextDB *mongo.Database, dbName string) {
+	hdxt := c.GetHDXT()
+
 	collection := plaintextDB.Collection("id_keywords")
-
-	// Create a cursor with no timeout and batch size of 3000
 	ctx := context.TODO()
+
 	opts := options.Find().SetNoCursorTimeout(true).SetBatchSize(3000)
 	cur, err := collection.Find(ctx, bson.D{}, opts)
 	if err != nil {
 		log.Fatal("Error getting collection:", err)
 	}
-
-	// Close cursor
 	defer cur.Close(ctx)
 
-	// Read all records from cursor
 	var idKeywords []bson.M
 	if err = cur.All(ctx, &idKeywords); err != nil {
 		log.Fatal("Error getting keywordIds:", err)
 	}
-	// 1. Setup Phase
-	log.Println("Setup Phase Start")
-	setupTime := time.Now()
+
 	idKeywordsSetup := idKeywords[:len(idKeywords)/2]
-	idList := make([]string, 0, len(idKeywords)/2)
-	volumeList := make([]int, 0, len(idKeywords)/2)
+	idList := make([]string, 0, len(idKeywordsSetup))
+	volumeList := make([]int, 0, len(idKeywordsSetup))
+	setupTimeList := make([]time.Duration, 0, len(idKeywordsSetup))
+
 	for _, idKeyword := range idKeywordsSetup {
-		valSet, ok := idKeyword["val_st"].(primitive.A)
-		if !ok {
-			log.Fatal("val_set is not of type primitive.A")
-		}
-		var keywords []string
-		for _, v := range valSet {
-			if str, ok := v.(string); ok {
-				keywords = append(keywords, str)
-			} else {
-				log.Fatal("val_set contains non-string value")
-			}
-		}
-		keywords = utils.RemoveDuplicates(keywords) // Deduplicate keywords
+		keywords := extractKeywords(idKeyword)
 		id := idKeyword["id"].(string)
+
 		encryptTime := time.Now()
 		_, err = hdxt.Setup(id, keywords, HDXT.Add)
 		if err != nil {
@@ -89,86 +92,61 @@ func main() {
 		idList = append(idList, id)
 		volumeList = append(volumeList, len(keywords))
 	}
-	// Send Setup request
-	log.Println("Sending Setup Request, cost time:", time.Since(setupTime))
+
+	// Send to server
 	c.Setup(hdxt.MitraCipherList, hdxt.AuhmeCipherList)
-	// save to file
-	saveTime := time.Now()
-	resultpath := filepath.Join("result", "Setup", "HDXT", fmt.Sprintf("%s.csv", saveTime.Format("2006-01-02_15-04-05")))
-	resultHeader := []string{"id", "volume", "addTime"}
-	resultData := make([][]string, len(idList))
-	for i, id := range idList {
-		resultData[i] = []string{id, strconv.Itoa(volumeList[i]), strconv.Itoa(int(setupTimeList[i].Microseconds()))}
-	}
-	err = utils.WriteResultToCSV(resultpath, resultHeader, resultData)
+
+	// Save results
+	saveResults("Setup", "HDXT", dbName, idList, volumeList, setupTimeList)
+}
+
+func UpdatePhase(c *client.HDXTClient, plaintextDB *mongo.Database, dbName string) {
+	collection := plaintextDB.Collection("id_keywords")
+	ctx := context.TODO()
+
+	opts := options.Find().SetNoCursorTimeout(true).SetBatchSize(3000)
+	cur, err := collection.Find(ctx, bson.D{}, opts)
 	if err != nil {
-		log.Fatal("Error writing result to file:", err)
+		log.Fatal("Error getting collection:", err)
+	}
+	defer cur.Close(ctx)
+
+	var idKeywords []bson.M
+	if err = cur.All(ctx, &idKeywords); err != nil {
+		log.Fatal("Error getting keywordIds:", err)
 	}
 
-	// 2. Update Phase
-	log.Println("Update Phase Start")
-	idList = make([]string, 0, len(idKeywords)/2)
 	idKeywordsUpdate := idKeywords[len(idKeywords)/2:]
-	volumeList = make([]int, 0, len(idKeywords)/2)
+	idList := make([]string, 0, len(idKeywordsUpdate))
+	volumeList := make([]int, 0, len(idKeywordsUpdate))
+	updateTimeList := make([]time.Duration, 0, len(idKeywordsUpdate))
+
 	for _, idKeyword := range idKeywordsUpdate {
-		valSet, ok := idKeyword["val_st"].(primitive.A)
-		if !ok {
-			log.Fatal("val_set is not of type primitive.A")
-		}
-		var keywords []string
-		for _, v := range valSet {
-			if str, ok := v.(string); ok {
-				keywords = append(keywords, str)
-			} else {
-				log.Fatal("val_set contains non-string value")
-			}
-		}
-		keywords = utils.RemoveDuplicates(keywords) // Deduplicate keywords
+		keywords := extractKeywords(idKeyword)
 		id := idKeyword["id"].(string)
+
 		encryptTime := time.Now()
 		err = c.Update(id, keywords, HDXT.Add)
 		if err != nil {
-			log.Fatal("Error in Encrypt:", err)
+			log.Fatal("Error in Update:", err)
 		}
 
-		// save to []
-		setupTimeList = append(setupTimeList, time.Since(encryptTime))
+		updateTimeList = append(updateTimeList, time.Since(encryptTime))
 		idList = append(idList, id)
 		volumeList = append(volumeList, len(keywords))
 	}
-	saveTime = time.Now()
 
-	// save to file
-	resultpath = filepath.Join("result", "Update", "HDXT", fmt.Sprintf("%s.csv", saveTime.Format("2006-01-02_15-04-05")))
-
-	// Define result header
-	resultHeader = []string{"id", "volume", "addTime"}
-
-	// Organize result data into tabular form
-	resultData = make([][]string, len(idList))
-	for i, id := range idList {
-		resultData[i] = []string{id, strconv.Itoa(volumeList[i]), strconv.Itoa(int(setupTimeList[i].Microseconds()))}
-	}
-
-	// Write results to file
-	err = utils.WriteResultToCSV(resultpath, resultHeader, resultData)
-	if err != nil {
-		log.Fatal("Error writing result to file:", err)
-	}
-
-	// 3. Search Phase
-	log.Println("Search Phase Start")
-	SearchPhase(c, *dbName, "keywords_2.txt")
+	// Save results
+	saveResults("Update", "HDXT", dbName, idList, volumeList, updateTimeList)
 }
 
-func SearchPhase(c *client.HDXTClient, tableName, fileName string) {
-	fileName = "./cmd/HDXT/configs/" + fileName
+func SearchPhase(c *client.HDXTClient, tableName string) {
+	fileName := "./cmd/HDXT/configs/keywords_2.txt"
 	keywordsList := utils.QueryKeywordsFromFile(fileName)
-	// Initialize result list
-	clientSearchTime := make([]time.Duration, 0, len(keywordsList)+1)
-	resultLengthList := make([]int, 0, len(keywordsList)+1)
 
-	// Search loop
+	clientSearchTime := make([]time.Duration, 0, len(keywordsList))
+	resultLengthList := make([]int, 0, len(keywordsList))
+
 	for _, keywords := range keywordsList {
 		searchTime := time.Now()
 		sIdList, err := c.Search(keywords)
@@ -176,27 +154,54 @@ func SearchPhase(c *client.HDXTClient, tableName, fileName string) {
 			log.Fatal("Error in Search:", err)
 		}
 
-		// Add results to result list
 		clientSearchTime = append(clientSearchTime, time.Since(searchTime))
 		resultLengthList = append(resultLengthList, len(sIdList))
 	}
-	log.Println("Search Phase End")
 
-	// Set result file path and name
+	// Save results
 	resultpath := filepath.Join("result", "Search", "HDXT", tableName, fmt.Sprintf("%s.csv", time.Now().Format("2006-01-02_15-04-05")))
-
-	// Define result header
 	resultHeader := []string{"keyword", "clientTime", "resultLength"}
-
-	// Organize result data into tabular form
 	resultData := make([][]string, len(keywordsList))
+
 	for i, keywords := range keywordsList {
 		resultData[i] = []string{strings.Join(keywords, "#"), strconv.Itoa(int(clientSearchTime[i].Microseconds())), strconv.Itoa(resultLengthList[i])}
 	}
 
-	// Write results to file
 	err := utils.WriteResultToCSV(resultpath, resultHeader, resultData)
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func extractKeywords(idKeyword bson.M) []string {
+	valSet, ok := idKeyword["val_st"].(primitive.A)
+	if !ok {
+		log.Fatal("val_set is not of type primitive.A")
+	}
+
+	var keywords []string
+	for _, v := range valSet {
+		if str, ok := v.(string); ok {
+			keywords = append(keywords, str)
+		} else {
+			log.Fatal("val_set contains non-string value")
+		}
+	}
+	return utils.RemoveDuplicates(keywords)
+}
+
+func saveResults(phase, scheme, dbName string, idList []string, volumeList []int, timeList []time.Duration) {
+	saveTime := time.Now()
+	resultpath := filepath.Join("result", phase, scheme, dbName, fmt.Sprintf("%s.csv", saveTime.Format("2006-01-02_15-04-05")))
+	resultHeader := []string{"id", "volume", "addTime"}
+	resultData := make([][]string, len(idList))
+
+	for i, id := range idList {
+		resultData[i] = []string{id, strconv.Itoa(volumeList[i]), strconv.Itoa(int(timeList[i].Microseconds()))}
+	}
+
+	err := utils.WriteResultToCSV(resultpath, resultHeader, resultData)
+	if err != nil {
+		log.Fatal("Error writing result to file:", err)
 	}
 }
